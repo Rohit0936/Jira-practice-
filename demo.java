f78290f32a238d7e131ce9460b0f9ef676f2b66c
Rohit0936 <boraderohit66@gmail.com>
2025-04-27 12:33:45 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo
+{
+  public static void main(String x[])
+  {
+      int a;
+      int b=2;
+      
+      a=a+b;
+      System.out.println(a);
+  }
+}
---
+++ demo.js
@@ -0,0 +1,4 @@
+let a=[1,2,3,4,5];
+
+a.splice(1,3,10,20,30);
+console.log(a);
